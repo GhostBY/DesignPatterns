0903261520c50d98c6473fefdb2d3644e0629aea
Lukashov Alexandr <alexandrlukashov@outlook.com>
2017-09-28 18:44:38 +0300

    Add and complte Singleton project

---
+++ Singletone/Earth.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    public class Earth
+    {
+        private static Earth instance;
+        protected Earth()
+        {
+            
+        }
+
+        public static Earth Instance()
+        {
+            if (instance == null)
+            {
+                instance= new Earth();
+            }
+            return instance;
+        }
+}
+}
---
+++ Singletone/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singletone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Earth earth1 = Earth.Instance();
+            Earth earth2 = Earth.Instance();
+            if (earth2 == earth1)
+            {
+                Console.WriteLine("Same objects");
+            }
+            else
+            {
+                Console.WriteLine("Bad work");
+            }
+            Console.ReadLine();
+        }
+    }
+}
